dbe04e7c7cc5b1d4d51c111874df41b7161adc50
unknown <you@example.com>
2024-10-01 10:10:17 +0200

    lancement du projet toDoList

---
+++ app/View/partial/footer.php
@@ -0,0 +1,8 @@
+</div>
+<footer class="text-center text-light myFooter pt-5">
+    <div class="text-center p-3">
+        © Copyright :Ghobrial Sarah <?php echo date("Y")?>
+    </div>
+</footer>
+</body>
+</html>
